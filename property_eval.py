bc858ac9f6515441298e70c5f4bc43273e75194b
Mateus C. Vieira <mateusv@email.com>
2021-03-29 18:10:01 -0300

    done

--- property_eval.py
+++ property_eval.py
@@ -1,179 +1,66 @@
-"""
-==========================================
-Fuzzy Control Systems: The Tipping Problem
-==========================================
-
-The 'tipping problem' is commonly used to illustrate the power of fuzzy logic
-principles to generate complex behavior from a compact, intuitive set of
-expert rules.
-
-If you're new to the world of fuzzy control systems, you might want
-to check out the `Fuzzy Control Primer
-<../userguide/fuzzy_control_primer.html>`_
-before reading through this worked example.
-
-The Tipping Problem
--------------------
-
-Let's create a fuzzy control system which models how you might choose to tip
-at a restaurant.  When tipping, you consider the service and food quality,
-rated between 0 and 10.  You use this to leave a tip of between 0 and 25%.
-
-We would formulate this problem as:
-
-* Antecednets (Inputs)
-   - `service`
-      * Universe (ie, crisp value range): How good was the service of the wait
-        staff, on a scale of 0 to 10?
-      * Fuzzy set (ie, fuzzy value range): poor, acceptable, amazing
-   - `food quality`
-      * Universe: How tasty was the food, on a scale of 0 to 10?
-      * Fuzzy set: bad, decent, great
-* Consequents (Outputs)
-   - `tip`
-      * Universe: How much should we tip, on a scale of 0% to 25%
-      * Fuzzy set: low, medium, high
-* Rules
-   - IF the *service* was good  *or* the *food quality* was good,
-     THEN the tip will be high.
-   - IF the *service* was average, THEN the tip will be medium.
-   - IF the *service* was poor *and* the *food quality* was poor
-     THEN the tip will be low.
-* Usage
-   - If I tell this controller that I rated:
-      * the service as 9.8, and
-      * the quality as 6.5,
-   - it would recommend I leave:
-      * a 20.2% tip.
-
-
-Creating the Tipping Controller Using the skfuzzy control API
--------------------------------------------------------------
-
-We can use the `skfuzzy` control system API to model this.  First, let's
-define fuzzy variables
-"""
 import matplotlib.pyplot as plt
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-# New Antecedent/Consequent objects hold universe variables and membership
-# functions
-quality = ctrl.Antecedent(np.arange(0, 11, 1), 'quality')
-service = ctrl.Antecedent(np.arange(0, 11, 1), 'service')
-tip = ctrl.Consequent(np.arange(0, 26, 1), 'tip')
+# Valores para teste:
+construction_standard_test_value = 8
+property_area_test_value = 205
 
-# Auto-membership function population is possible with .automf(3, 5, or 7)
-quality.automf(3)
-service.automf(3)
+# Objetos com ranges:
+property_area = ctrl.Antecedent(np.arange(20, 351, 0.5), 'property_area')
+construction_standard = ctrl.Antecedent(np.arange(0, 11, 0.01), 'construction_standard')
+price = ctrl.Consequent(np.arange(50000, 1000001, 100), 'price')
 
-# Custom membership functions can be built interactively with a familiar,
-# Pythonic API
-tip['low'] = fuzz.trimf(tip.universe, [0, 0, 13])
-tip['medium'] = fuzz.trimf(tip.universe, [0, 13, 25])
-tip['high'] = fuzz.trimf(tip.universe, [13, 25, 25])
+# Funções de pertinência automática:
 
-"""
-To help understand what the membership looks like, use the ``view`` methods.
-"""
+property_area.automf(5, names=['tiny', 'small', 'medium', 'large', 'huge'])
+construction_standard.automf(5, names=['very low', 'low', 'average', 'high', 'very high'])
+price.automf(5, names=['very cheap', 'cheap', 'fair', 'expensive', 'very expensive'])
 
-# You can see how these look with .view()
-quality['average'].view()
+# Regras:
 
+rule1 = ctrl.Rule(property_area['tiny'] & construction_standard['very low'], price['very cheap'])
+rule2 = ctrl.Rule(property_area['tiny'] & construction_standard['low'], price['very cheap'])
+rule3 = ctrl.Rule(property_area['tiny'] & construction_standard['average'], price['cheap'])
+rule4 = ctrl.Rule(property_area['tiny'] & construction_standard['high'], price['cheap'])
+rule5 = ctrl.Rule(property_area['tiny'] & construction_standard['very high'], price['fair'])
 
-"""
-.. image:: PLOT2RST.current_figure
-"""
-service.view()
+rule6 = ctrl.Rule(property_area['small'] & construction_standard['very low'], price['very cheap'])
+rule7 = ctrl.Rule(property_area['small'] & construction_standard['low'], price['cheap'])
+rule8 = ctrl.Rule(property_area['small'] & construction_standard['average'], price['cheap'])
+rule9 = ctrl.Rule(property_area['small'] & construction_standard['high'], price['fair'])
+rule10 = ctrl.Rule(property_area['small'] & construction_standard['very high'], price['fair'])
 
-"""
-.. image:: PLOT2RST.current_figure
-"""
-tip.view()
-"""
-.. image:: PLOT2RST.current_figure
+rule11 = ctrl.Rule(property_area['medium'] & construction_standard['very low'], price['cheap'])
+rule12 = ctrl.Rule(property_area['medium'] & construction_standard['low'], price['fair'])
+rule13 = ctrl.Rule(property_area['medium'] & construction_standard['average'], price['fair'])
+rule14 = ctrl.Rule(property_area['medium'] & construction_standard['high'], price['expensive'])
+rule15 = ctrl.Rule(property_area['medium'] & construction_standard['very high'], price['expensive'])
 
+rule16 = ctrl.Rule(property_area['large'] & construction_standard['very low'], price['fair'])
+rule17 = ctrl.Rule(property_area['large'] & construction_standard['low'], price['fair'])
+rule18 = ctrl.Rule(property_area['large'] & construction_standard['average'], price['expensive'])
+rule19 = ctrl.Rule(property_area['large'] & construction_standard['high'], price['very expensive'])
+rule20 = ctrl.Rule(property_area['large'] & construction_standard['very high'], price['very expensive'])
 
-Fuzzy rules
------------
+rule21 = ctrl.Rule(property_area['huge'] & construction_standard['very low'], price['fair'])
+rule22 = ctrl.Rule(property_area['huge'] & construction_standard['low'], price['expensive'])
+rule23 = ctrl.Rule(property_area['huge'] & construction_standard['average'], price['very expensive'])
+rule24 = ctrl.Rule(property_area['huge'] & construction_standard['high'], price['very expensive'])
+rule25 = ctrl.Rule(property_area['huge'] & construction_standard['very high'], price['very expensive'])
 
-Now, to make these triangles useful, we define the *fuzzy relationship*
-between input and output variables. For the purposes of our example, consider
-three simple rules:
+controller = ctrl.ControlSystem([eval("rule" + str(x)) for x in range(1,26)])
+simulator = ctrl.ControlSystemSimulation(controller)
 
-1. If the food is poor OR the service is poor, then the tip will be low
-2. If the service is average, then the tip will be medium
-3. If the food is good OR the service is good, then the tip will be high.
-
-Most people would agree on these rules, but the rules are fuzzy. Mapping the
-imprecise rules into a defined, actionable tip is a challenge. This is the
-kind of task at which fuzzy logic excels.
-"""
-
-rule1 = ctrl.Rule(quality['poor'] | service['poor'], tip['low'])
-rule2 = ctrl.Rule(service['average'], tip['medium'])
-rule3 = ctrl.Rule(service['good'] | quality['good'], tip['high'])
-
-#rule1.view()
-
-"""
-.. image:: PLOT2RST.current_figure
-
-Control System Creation and Simulation
----------------------------------------
-
-Now that we have our rules defined, we can simply create a control system
-via:
-"""
-
-tipping_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
-
-"""
-In order to simulate this control system, we will create a
-``ControlSystemSimulation``.  Think of this object representing our controller
-applied to a specific set of cirucmstances.  For tipping, this might be tipping
-Sharon at the local brew-pub.  We would create another
-``ControlSystemSimulation`` when we're trying to apply our ``tipping_ctrl``
-for Travis at the cafe because the inputs would be different.
-"""
-
-tipping = ctrl.ControlSystemSimulation(tipping_ctrl)
-
-"""
-We can now simulate our control system by simply specifying the inputs
-and calling the ``compute`` method.  Suppose we rated the quality 6.5 out of 10
-and the service 9.8 of 10.
-"""
-# Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
-# Note: if you like passing many inputs all at once, use .inputs(dict_of_data)
-tipping.input['quality'] = 2.3
-tipping.input['service'] = 7.4
+simulator.input['construction_standard'] = construction_standard_test_value
+simulator.input['property_area'] = property_area_test_value
 
 # Crunch the numbers
-tipping.compute()
+simulator.compute()
 
-"""
-Once computed, we can view the result as well as visualize it.
-"""
-
-
-print(tipping.output['tip'])
-tip.view(sim=tipping)
+print(simulator.output['price'])
+property_area.view()
+construction_standard.view()
+price.view(sim=simulator)
 plt.show()
-"""
-.. image:: PLOT2RST.current_figure
-
-The resulting suggested tip is **20.24%**.
-
-Final thoughts
---------------
-
-The power of fuzzy systems is allowing complicated, intuitive behavior based
-on a sparse system of rules with minimal overhead. Note our membership
-function universes were coarse, only defined at the integers, but
-``fuzz.interp_membership`` allowed the effective resolution to increase on
-demand. This system can respond to arbitrarily small changes in inputs,
-and the processing burden is minimal.
-
-"""
